3f56b8d2f756a2694793f060e57665df30e0b4b8
sh0ut <sh0ut>
2024-11-26 14:50:37 +0200

    Rope for hook

--- Global.gd
+++ Global.gd
@@ -5,8 +5,10 @@ var level = null
 var world = null
 var gui  = null
 var hook : CharacterBody3D = null
+var hook_current_point : Transform3D
 
 signal hook_signal
+signal hook_current_position
 
 func stop():
 	get_tree().quit()
